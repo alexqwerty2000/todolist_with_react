e4e243f445af3abb7a3bf7bb4166e1806c4ac639
Alex Ivanov <ivanov.alex1@gmail.com>
2019-03-05 12:44:03 +0200

    add pages on branch add pages

---
+++ src/components/pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Pages extends Component{
+    render(){
+        return (
+               <div className="footer">
+                   <h6 className="">Copyright by Alex</h6>
+               </div>
+            )
+    }
+}
+export default Pages
